cf512f3188d9b1d98ad40f44aded8e6262a4701e
AvirajJagtap <115860231+AvirajJagtap@users.noreply.github.com>
2022-10-15 13:06:06 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+
+public class pattern1
+{
+	
+	public static void printStars(int n)
+	{
+		int i, j;
+
+		
+		for(i=0; i<n; i++)
+		{
+
+			
+			for(j=0; j<=i; j++)
+			{
+				
+				System.out.print("* ");
+			}
+
+		
+			System.out.println();
+		}
+}
+
+	
+	public static void main(String args[])
+	{
+		int n = 5;
+		printStars(n);
+	}
+}
