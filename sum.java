aa4bfba79f6b707ffd8174aec359d5a6543b070c
ShivamVishw <106417521+ShivamVishw@users.noreply.github.com>
2022-10-15 09:45:42 +0530

    Sum of number

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+
+    //find the sum of two number 
+    public static void main(String[] args) {
+        int number1= 4;
+        int number2= 5
+        int sum = number1 + number2;
+        System.out.println(sum);
+    }
+}
