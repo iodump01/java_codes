685ac3916eb25816c1e34ea1beda3c8f8764c22f
Omkar Ambalal Kodmur <76892682+Omkar-code-mur@users.noreply.github.com>
2022-10-15 13:07:17 +0530

    Update xPown.java

--- xPown.java
+++ xPown.java
@@ -4,13 +4,24 @@ public class xPown {
     //base case at oth and 1th
     
     public static int calcPower(int X, int n){
+        if(X == 0){
+            return 0;
+        }
+        if(n == 0){
+            return 1;
+        }
         //operation
         int xPownm_1= calcPower(X, n-1);
         int xPown= X*xPownm_1;
+        
         return xPown;
     }
     public static void main(String[] args) {
+        int ans1= calcPower(2,5);
+        int ans2= calcPower(2,7);
         int ans= calcPower(2,0);
         System.out.println(ans);
+        System.out.println(ans2);
+        System.out.println(ans1);
     }
 }
